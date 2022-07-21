07cce2ac3ad1525c51f9d890b641fecece0482ca
TylerN93 <tylernewton73@gmail.com>
2022-07-21 17:27:50 -0500

    initial commit

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+           
+            var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than 9";
+
+            Console.WriteLine(response);
+        }
+    }
+}
